9db7c7e63e0a654758a77246e74dc22d456a663a
Thomas Johnson <tjohnson1106@gmail.com>
2017-05-15 17:02:11 -0400

    added search term and news feed reducers

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,3 @@
+export const LOAD_NEWS = 'LOAD_NEWS';
+export cosnt SEARCH_NEWS = 'SEACRH_NEWS';
+
---
+++ src/reducers/searchTermReducer.js
@@ -0,0 +1,11 @@
+import { SEARCH_NEWS } from '../actions/actionTypes';
+export default (state = '', action = {}) => {
+    switch (action.type) {
+        case SEARCH_NEWS:
+            return action.payload;
+        default:
+            return state;
+    }
+};
+
+
